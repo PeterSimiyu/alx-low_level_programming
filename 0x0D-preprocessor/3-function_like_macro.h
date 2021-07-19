cabc23a627ee378d4ea5d2c7a50073df0c6ee3e8
PeterSimiyu <simiyupeter10@gmail.com>
2021-07-19 09:54:34 -0700

    fixed

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
 
 #define ABS(x) ((x) < 0 ? (-(x) : (x))
 typedef int make_iso_compilers_happy;
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif /* FUNCTION_LIKE_MACRO_H_ */
