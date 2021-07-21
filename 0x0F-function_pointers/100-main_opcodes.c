2d4fa17f63a720d200ac218c9a7ae148c9af02bd
PeterSimiyu <simiyupeter10@gmail.com>
2021-07-21 11:18:55 -0700

    extra

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+
+
+/*
+ *Interesting note: functions are a set of instructions and
+ *function pointers are pointers to the beginning of this instruction
+ *so if we could get the first instructions adress inmemory
+ *we can see what it is doing by printing the adresses
+ */
+
+/**
+ *main - print opcodes of its own main func
+ *@argc: number of arguments
+ *@argv: vector of arguments
+ *Return: 0 if no errors
+ */
+int main(int argc, char *argv[])
+{
+	register int i, num;
+	char *ptr = (char *)main;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	num = atoi(argv[1]);
+	if (num < 0)
+		printf("Error\n"), exit(2);
+	for (i = 0; i < num - 1; i++)
+		printf("%02hhx\n", ptr[i]);
+	return (0);
+}
