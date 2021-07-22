22529a934090c71cb4f251ea99a27adc1e06b95e
PeterSimiyu <simiyupeter10@gmail.com>
2021-07-22 10:39:49 -0700

    fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,20 +1,21 @@
 #include "3-calc.h"
 /**
- *op_add - adds two integers
- *@a: first integer
- *@b: second integer
- *Return: sum of two integers
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of two integers
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+
 /**
- *op_sub - subtracts two integers
- *@a: first integer
- *@b: second integer
- *Return: difference of two integers
+ * op_sub - subtracts two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of two integers
  */
 int op_sub(int a, int b)
 {
@@ -22,10 +23,10 @@ int op_sub(int a, int b)
 }
 
 /**
- *op_mul - multiply two integers
- *@a: first integer
- *@b: second integer
- *Return: product of two integers
+ * op_mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
  */
 int op_mul(int a, int b)
 {
@@ -33,10 +34,10 @@ int op_mul(int a, int b)
 }
 
 /**
- *op_div - divides two integers
- *@a: first integer
- *@b: second integer
- *Return: quotient of two integers
+ * op_div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: quotient of two integers
  */
 int op_div(int a, int b)
 {
@@ -49,10 +50,10 @@ int op_div(int a, int b)
 }
 
 /**
- *op_mod - calculates modulus two integers
- *@a: first integer
- *@b: second integer
- *Return: modulus of two integers
+ * op_mod - calculates modulus two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus of two integers
  */
 int op_mod(int a, int b)
 {
