df0ab111d8afa2f7616e6848c58e54322ec135f3
PeterSimiyu <simiyupeter10@gmail.com>
2021-07-22 10:40:12 -0700

    fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 
 /**
- *main - performa simple operations
- *@argc: argument count
- *@argv: argument vector
- *Return: 0 if no errors
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
  */
 int main(int argc, char *argv[])
 {
