3afdff9fe948c780f0c50d564863bd58f17e3af1
PeterSimiyu <simiyupeter10@gmail.com>
2021-08-03 00:19:43 -0700

    add fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,42 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: double pointer to head of list
- * @idx: index to add new node
- * @n: value at new node
- * Return: address of new node, or NULL if failed
+ * insert_nodeint_at_index -  inserts a new node at a given position.
+ * @head: double pointer to the first node in the list
+ * @idx: index of the node to insert
+ * @n: value for new node
+ *
+ * Return: pointer to the indexed node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	listint_t *newNode;
+	unsigned int i;
+	listint_t *current, *new;
 
-	register uint i;
-
-	newNode = malloc(sizeof(listint_t));
-	if (!head || !newNode)
+	if (head == NULL)
 		return (NULL);
-	newNode->n = n;
-	if (!idx)
+	if (idx != 0)
 	{
-		newNode->next = *head;
-		*head = newNode;
-		return (newNode);
-	}
-	current = *head;
-	while (current)
-	{
-		if (i == idx - 1)
+		current = *head;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
 		{
-			newNode->next = current->next;
-			current->next = newNode;
-			return (newNode);
+			current = current->next;
 		}
-		i++;
-		current = current->next;
+		if (current == NULL)
+			return (NULL);
 	}
-	free(newNode);
-	return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
