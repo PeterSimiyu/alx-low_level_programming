42c1b88f317011e0cdee0517424eb70a4faf2a5e
PeterSimiyu <simiyupeter10@gmail.com>
2021-08-05 10:09:19 -0700

    update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,13 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define  BIT_SIZE 8
-
-int _putchar(char c);
+int _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -15,7 +9,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int _strlen(const char *s);
-_Bool isBinary(char);
 
-#endif /* HOLBERTON_H */
+#endif /* _HOLBERTON_H_ */
